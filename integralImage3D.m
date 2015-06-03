f90c6c6fa681aa6c783621c24e1c408cf756115f
carterbox <daniel.j.ching@gmail.com>
2015-06-03 12:58:43 -0700

    Documented and completed simple testing.

--- integralImage3D.m
+++ integralImage3D.m
@@ -3,22 +3,26 @@ function [J] = integralImage3D(V)
 % whose indecies contain the sum of all the intensities in the original image
 % that are above and to the left. This one is inclusive and 1 indexed.
 
-[u1,u2,u3] = size(V);
-% pad the integral image with zeros to make the loop less complicated
-J = zeros(size(V) + 1);
+V = double(V); % We need the extra bit depth for large volumes.
+               % Example: 128*1024^3 ~ 1.3x10^11 > 2^32
+               % Also, Matlab processes double faster than int64.
 
-for i = 2:u1
-for j = 2:u2
-for k = 2:u3
+[u1,u2,u3] = size(V);
+% Pad the integral image with zeros to make the loop simpler (There's no
+% need for special cases at the edges). Make the default value -1 for
+% troubleshooting purposes.
+J = padarray(-ones(u1,u2,u3, 'double'), [1,1,1], 'pre');
+for i = 2:u1+1
+for j = 2:u2+1
+for k = 2:u3+1
     
     % Single pass integral image computation extrapolated to 3D from
     % http://en.wikipedia.org/wiki/Summed_area_table
     
-    J(i,j,k) = V(i,j,k) + J(i-1,j,k) + J(i,j-1,k) + J(i,j,k-1)...
+    J(i,j,k) = V(i-1,j-1,k-1) + J(i-1,j,k) + J(i,j-1,k) + J(i,j,k-1)...
     - J(i,j-1,k-1) - J(i-1,j,k-1) - J(i-1,j-1,k) + J(i-1,j-1,k-1);
 
 end
 end
 end
-
 end
--- sumintegralimage3D.m
+++ sumintegralimage3D.m
@@ -1,25 +1,44 @@
-function [T] = sumintegralimage3D(corner,size,J)
-% SUMINTEGRALIMAGE uses integral image J to calculate the integral over an
-% area in an image defined by the coordinates CORNER and vector SIZE.
+function [T] = sumintegralimage3D(corner,boxsize,J)
+% SUMINTEGRALIMAGE3D uses integral image J to calculate the integral over
+% an area in an image defined by the coordinates CORNER and vector BOXSIZE.
+%
+% INPUTS
+% corner: the corner of the box closest to [0,0,0].
+% boxsize: a vector containing the side lengths of the box.
+% J: the integral image of some volume.
+%
+% OUTPUTS
+% T: The sum of intensities of the points in the area of the represented
+% volume.
+%
+% NOTES:
+% http://en.wikipedia.org/wiki/Summed_area_table
+% ------------------------------------------------------------------------
 
 % Check for 3D vectors.
-assert(length(corner) == 3 && length(size) == 3);
-
+assert(length(corner) == 3 && length(boxsize) == 3);
 % Make sure the size of the area is not larger than J.
-%assert()
+assert(sum(sum(size(J) - boxsize)) >= 0);
+% Convert to row vectors.
+if(iscolumn(corner)), corner = corner'; end;
+if(iscolumn(boxsize)), boxsize = boxsize'; end;
 
 % Define the corners of the cube in the integral image.
 G = corner;
-A = G + size;
-B = G + [0 1 1].*size;
-C = G + [0 0 1].*size;
-D = G + [1 0 1].*size;
-E = G + [1 1 0].*size;
-F = G + [0 1 0].*size;
-H = G + [1 0 0].*size;
+A = G + boxsize;
+B = G + [0 1 1].*boxsize;
+C = G + [0 0 1].*boxsize;
+D = G + [1 0 1].*boxsize;
+E = G + [1 1 0].*boxsize;
+F = G + [0 1 0].*boxsize;
+H = G + [1 0 0].*boxsize;
 
 % Add and subtract the corners according to this idea on Wikipedia
 % http://en.wikipedia.org/wiki/Summed_area_table but extrapolated to 3D
-T = J(A) - J(E) - J(D) - J(B) + J(C) + J(H) + J(F) - 3*J(G);
+% T = A - E - D - B + C + H + F - G
+T = J(A(1),A(2),A(3))...
+    - J(E(1),E(2),E(3)) - J(D(1),D(2),D(3)) - J(B(1),B(2),B(3))...
+    + J(C(1),C(2),C(3)) + J(H(1),H(2),H(3)) + J(F(1),F(2),F(3))...
+    - J(G(1),G(2),G(3));
 
 end
