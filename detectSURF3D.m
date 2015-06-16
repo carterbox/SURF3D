e604c5003e6cf11968412d6322ec861a3fcf86a3
carterbox <daniel.j.ching@gmail.com>
2015-06-16 14:28:28 -0700

    Organizing files.

--- detectSURF3D.m
+++ detectSURF3D.m
@@ -1,5 +1,5 @@
 function [points] = detectSURF3D(V)
-% DETECTSURF3D detects SURF points in a volume. #childparallel
+% DETECTSURF3D detects SURF points in a volume. #parallelchild
 %
 % INPUTS
 % V: the input volume.
@@ -9,12 +9,13 @@ function [points] = detectSURF3D(V)
 % row vector [x,y,z,scale,saliency].
 %
 % NOTES
-% [citations here]
-% For an octive size of 5, is it more efficient to pad once or pad for
-% each filter separately?
+% This work is an extension of the work by Bay, Herbert, et al. "Speeded-up
+% robust features (SURF)." Computer vision and image understanding 110.3 
+% (2008): 346-359.
+%
 %% -----------------------------------------------------------------------
 log = 1; % 1 is the default terminal.
-koctaves = 1; nbhood = [4,4,4,4];
+koctaves = 1; nbhood = [3,3,3,3];
 octaves = {[9,15,21,27]};
 %[27,33,39,45],[39,51,63,75],[27,51,75,99],[51,99,147,195]};
 
@@ -22,6 +23,8 @@ octaves = {[9,15,21,27]};
 R = cell(9,1); % Store the results in cell R.
 for i = 1:koctaves
     % Pad the array for the largest filter in the octave.
+    % For an octive size of 5, is it more efficient to pad once or pad for
+    % each filter separately?
     buffer = repmat((max(octaves{i})-1)/2,[1,3]);
     padded_V = padarray(V,buffer,'replicate','both');
     J = integralimage3D(padded_V);
@@ -30,7 +33,7 @@ for scale = octaves{i}
     % Check to see if this scale has already been computed.
     filename = sprintf('Rscale%02i.mat',scale);
     if(exist(filename, 'file') > 0)
-        fprintf(log, ' X');
+        fprintf(log, ' L');
         load(filename,'detH');
         R{scale} = detH;
     else
@@ -41,14 +44,12 @@ for scale = octaves{i}
            detH = padded_detH(1+buffer:end-buffer,...
                               1+buffer:end-buffer,...
                               1+buffer:end-buffer);
-
            save(filename,'detH');
            R{scale} = detH;
         else
            fprintf(log, ' X');
         end
     end
-    fprintf('\n');
 end
 fprintf(' DONE.');
 end
@@ -69,8 +70,7 @@ for i = 1:koctaves
     maxima{i} = refinepeaks(maxima{i},A,4,octaves{i});
     fprintf(' DONE.');
 end
-
-%% Combine all the results and put strongest responses first.
+% Combine all the results and put strongest responses first.
 points = maxima{1};
 for i = 2:length(maxima), points = cat(1,points,maxima{i}); end
 points = sortrows(points, -5); % reponse magnitude is col 5.
